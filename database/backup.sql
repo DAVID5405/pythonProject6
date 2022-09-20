9c22ba21b8d8a8b1d6874282563df8e012db4fae
David Katreniak <david.00480@gmail.com.com>
2022-09-20 08:39:12 +0200

    commit

---
+++ database/backup.sql
@@ -0,0 +1,11 @@
+-- auto-generated definition
+create table data
+(
+    ID           int auto_increment
+        primary key,
+    location     varchar(255) null,
+    locationTime varchar(255) null,
+    info         varchar(255) null,
+    weather      varchar(255) null
+);
+
